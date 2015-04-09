28ca6ffef3f2f09f8ef68db525b6e868d003eac4
yrsegal <yrsegal@gmail.com>
2015-04-09 10:14:35 -0400

    Add extra filter

--- cloudtobutt.js
+++ cloudtobutt.js
@@ -31,6 +31,8 @@ function handleText(textNode)  {
 	v = v.replace(/\bThe cloud\b/g, "My butt");
     v = v.replace(/\bthe Cloud\b/g, "my Butt");
 	v = v.replace(/\bthe cloud\b/g, "my butt");
+    v = v.replace(/\bCloud\b/g, "Butt");
+	v = v.replace(/\bcloud\b/g, "butt");
 	
 	textNode.nodeValue = v;
 }
